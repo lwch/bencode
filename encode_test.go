9d56a4131cd0cb5657ba7f5ab6478b1e66aa8f19
lwch <liwenchao@jkstack.com>
2021-01-15 12:00:52 +0800

    完善test case

--- encode_test.go
+++ encode_test.go
@@ -2,12 +2,12 @@ package bencode
 
 import (
 	"bytes"
-	"encoding/hex"
-	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestEncode(t *testing.T) {
+	// example of http://www.bittorrent.org/beps/bep_0005.html
 	var obj struct {
 		T string `bencode:"t"`
 		Y string `bencode:"y"`
@@ -19,11 +19,29 @@ func TestEncode(t *testing.T) {
 	obj.T = "aa"
 	obj.Y = "q"
 	obj.Q = "ping"
-	copy(obj.A.ID[:], []byte("abcd"))
+	copy(obj.A.ID[:], []byte("abcdefghij0123456789"))
 	var buf bytes.Buffer
 	err := NewEncoder(&buf).Encode(obj)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("FATAL: encode %v", err)
 	}
-	fmt.Println(hex.Dump(buf.Bytes()))
+	data := buf.String()
+	if data[0] != 'd' {
+		t.Fatalf("unexpected first char: %c", data[0])
+	}
+	if data[len(data)-1] != 'e' {
+		t.Fatalf("unexpected end char: %c", data[len(data)-1])
+	}
+	if !strings.Contains(data, "1:ad2:id20:abcdefghij0123456789e") {
+		t.Fatal("value of key a not found")
+	}
+	if !strings.Contains(data, "1:q4:ping") {
+		t.Fatal("value of key q not found")
+	}
+	if !strings.Contains(data, "1:t2:aa") {
+		t.Fatal("value of key t not found")
+	}
+	if !strings.Contains(data, "1:y1:q") {
+		t.Fatal("value of key y not found")
+	}
 }
