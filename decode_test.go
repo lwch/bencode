65136e36f2e8699f5e3941afc301999bc211f079
lwch <liwenchao@jkstack.com>
2021-01-15 15:15:52 +0800

    增加decode test case

--- decode_test.go
+++ decode_test.go
@@ -1,11 +1,12 @@
 package bencode
 
 import (
-	"fmt"
+	"bytes"
 	"testing"
 )
 
 func TestDecode(t *testing.T) {
+	// example of http://www.bittorrent.org/beps/bep_0005.html
 	str := "d1:ad2:id20:abcdefghij0123456789e1:q4:ping1:t2:aa1:y1:qe"
 	var obj struct {
 		T string `bencode:"t"`
@@ -19,5 +20,16 @@ func TestDecode(t *testing.T) {
 	if err != nil {
 		t.Fatalf("FATAL: decode: %v", err)
 	}
-	fmt.Println(obj)
+	if obj.T != "aa" {
+		t.Fatalf("unexpected value of t: %s", obj.T)
+	}
+	if obj.Y != "q" {
+		t.Fatalf("unexpected value of y: %s", obj.Y)
+	}
+	if obj.Q != "ping" {
+		t.Fatalf("unexpected value of q: %s", obj.Q)
+	}
+	if bytes.Compare(obj.A.ID[:], []byte("abcdefghij0123456789")) != 0 {
+		t.Fatalf("unexpected value of a.id: %s", string(obj.A.ID[:]))
+	}
 }
